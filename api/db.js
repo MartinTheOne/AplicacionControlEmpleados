59d0bd547045818b4d310bbcf830b79971cb1f52
Otro <gonzalezmartinnatanael@gmail.com>
2025-01-07 00:49:44 -0300

    variable de entorno

--- api/db.js
+++ api/db.js
@@ -1,10 +1,9 @@
 const { MongoClient } = require('mongodb');
 
-
 let client;
 let clientPromise;
 
-const uri ='mongodb+srv://gonzalezmartinnatanael:1Tbbr7KZOnP77SM7@cluster0.r5te7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const uri =process.env.MONGODB_URI;
 
 if (!client) {
   client = new MongoClient(uri);
